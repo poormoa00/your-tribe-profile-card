493534897725e2dee9ea59aead015300cc484fa5
poormoa00 <aria.poormohsein@hva.nl>
2024-09-02 12:44:30 +0200

    naam, naamkleur, achtegrond kleur, omgang van de achtergrond kleur, hobby's, letterkleuren alles zwart, eigen foto.
    ik heb dit naar mijn eigen smaak veranderd.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button') //dit is de knop om het te vergroten
+expandButton.addEventListener('click', expand)    //dit vergroot het visite kaartje
 
 function expand () {
-  document.body.classList.toggle('expand')
+  document.body.classList.toggle('expand')  //dit is de functie dat als je erop drukt dat het vergoot 
 }
