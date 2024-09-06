677455c97cb2f9a406a3aa56d47e37e1a43fa52a
poormoa00 <aria.poormohsein@hva.nl>
2024-09-06 09:47:14 +0200

    Meest recente versie van mijn visite kaartje

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,57 @@
-const expandButton = document.querySelector('header button') //dit is de knop om het te vergroten
-expandButton.addEventListener('click', expand)    //dit vergroot het visite kaartje
+//const expandButton = document.querySelector('header button') //dit is de knop om het te vergroten
+//expandButton.addEventListener('click', expand)    //dit vergroot het visite kaartje
 
-function expand () {
-  document.body.classList.toggle('expand')  //dit is de functie dat als je erop drukt dat het vergoot 
+//function expand () {
+  //document.querySelector("p").textContent = 'oude student fdnd'  //dit is de functie dat als je erop drukt dat het vergoot 
+
+let btn = document.querySelectorAll("button");
+let detail = document.querySelectorAll("span");
+
+
+
+let cleanbtn = function () {
+    btn.forEach(function (element) {
+        element.classList.remove("active");
+    });
 }
+
+
+let cleaninfo = function () {
+  detail.forEach(function (element) {
+    element.classList.remove("active");
+});
+}
+
+let setInfo = function (i) {
+  detail[i].classList.add("active");
+}
+
+btn.forEach(function(element, index ){
+element.addEventListener("click",function(evt){
+cleanbtn();
+element.classList.add("active");
+
+cleaninfo();
+
+switch(index){
+  case 0: setInfo(0)
+  break;
+
+  case 1: setInfo(2)
+  break;
+
+  case 2: setInfo(0);
+  setInfo(1);
+  setInfo(2);
+  setinfo(3)
+  break;
+  
+  case 3: setInfo(1);
+  break;
+
+  case 4: setInfo(2);
+  setinfo(3)
+  break;
+}
+});
+});
